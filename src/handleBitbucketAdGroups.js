60910e6c2e68d9648343d4d35d420cb3ec5aa387
Robert Schuh <robert.schuh@valtech.com>
2022-07-13 17:18:53 +0200

    add oidc mappings

--- src/handleBitbucketAdGroups.js
+++ src/handleBitbucketAdGroups.js
@@ -3,137 +3,68 @@ const log = require("./log");
 module.exports = (SERVER_URL, API_KEY, SONAR_HOST_URL, SONAR_LOGIN) =>
   async function (groups, teamId, adGroupCns) {
     for (let group of groups) {
-      await handleLdapGroup(
-        SERVER_URL,
-        API_KEY,
-        SONAR_HOST_URL,
-        SONAR_LOGIN,
-        groups,
-        teamId,
-        adGroupCns,
-        group
+      const adGroupsRes = await fetch(
+        `${SERVER_URL}/api/v1/ldap/groups?searchText=${encodeURIComponent(
+          group.group.name
+        )}&pageSize=1&pageNumber=1`,
+        {
+          method: "GET",
+          headers: {
+            ["x-api-key"]: API_KEY,
+          },
+        }
       );
-      await handleOidcGroup(
-        SERVER_URL,
-        API_KEY,
-        SONAR_HOST_URL,
-        SONAR_LOGIN,
-        groups,
-        teamId,
-        adGroupCns,
-        group
-      );
+      const adGroups = await adGroupsRes.json();
+      if (adGroups.length === 0) {
+        log("no ldap group with name found", group.group.name);
+        return;
+      }
+      await fetch(`${SERVER_URL}/api/v1/ldap/mapping`, {
+        method: "PUT",
+        headers: {
+          ["x-api-key"]: API_KEY,
+          ["Content-Type"]: "application/json",
+        },
+        body: JSON.stringify({ team: teamId, dn: adGroups[0] }),
+      });
+      const cnEl = adGroups[0]
+        .split(",")
+        .map((el) => el.split("="))
+        .find((el) => el[0] === "CN");
+      if (!cnEl) {
+        return;
+      }
+      const cn = cnEl[1];
+      adGroupCns.push(cn);
+      // create dtrack oidc group
+      const createOidcTeamRes = await fetch(`${SERVER_URL}/api/v1/oidc/group`, {
+        method: "PUT",
+        headers: {
+          ["x-api-key"]: API_KEY,
+          ["Content-Type"]: "application/json",
+        },
+        body: JSON.stringify({ name: cn }),
+      });
+      if (createOidcTeamRes.status === 201) {
+        const createOidcBody = await createOidcTeamRes.json();
+        await fetch(`${SERVER_URL}/api/v1/oidc/mapping`, {
+          method: "PUT",
+          headers: {
+            ["x-api-key"]: API_KEY,
+            ["Content-Type"]: "application/json",
+          },
+          body: JSON.stringify({ team: teamId, group: createOidcBody.uuid }),
+        });
+      }
+      // create sonar group
+      await fetch(`${SONAR_HOST_URL}/api/user_groups/create`, {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+          Authorization:
+            "Basic " + Buffer.from(SONAR_LOGIN + ":").toString("base64"),
+        },
+        body: `description=&name=${cn}`,
+        method: "POST",
+      });
     }
   };
-
-async function handleLdapGroup(
-  SERVER_URL,
-  API_KEY,
-  SONAR_HOST_URL,
-  SONAR_LOGIN,
-  groups,
-  teamId,
-  adGroupCns,
-  group
-) {
-  const adGroupsRes = await fetch(
-    `${SERVER_URL}/api/v1/ldap/groups?searchText=${encodeURIComponent(
-      group.group.name
-    )}&pageSize=1&pageNumber=1`,
-    {
-      method: "GET",
-      headers: {
-        ["x-api-key"]: API_KEY,
-      },
-    }
-  );
-  const adGroups = await adGroupsRes.json();
-  if (adGroups.length === 0) {
-    log("no ldap group with name found", group.group.name);
-    return;
-  }
-  await fetch(`${SERVER_URL}/api/v1/ldap/mapping`, {
-    method: "PUT",
-    headers: {
-      ["x-api-key"]: API_KEY,
-      ["Content-Type"]: "application/json",
-    },
-    body: JSON.stringify({ team: teamId, dn: adGroups[0] }),
-  });
-  const cnEl = adGroups[0]
-    .split(",")
-    .map((el) => el.split("="))
-    .find((el) => el[0] === "CN");
-  if (!cnEl) {
-    return;
-  }
-  const cn = cnEl[1];
-  adGroupCns.push(cn);
-  // create sonar group
-  await fetch(`${SONAR_HOST_URL}/api/user_groups/create`, {
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-      Authorization:
-        "Basic " + Buffer.from(SONAR_LOGIN + ":").toString("base64"),
-    },
-    body: `description=&name=${cn}`,
-    method: "POST",
-  });
-}
-
-async function handleOidcGroup(
-  SERVER_URL,
-  API_KEY,
-  SONAR_HOST_URL,
-  SONAR_LOGIN,
-  groups,
-  teamId,
-  adGroupCns,
-  group
-) {
-  const adGroupsRes = await fetch(
-    `${SERVER_URL}/api/v1/oidc/group?searchText=${encodeURIComponent(
-      group.group.name
-    )}&pageSize=1&pageNumber=1`,
-    {
-      method: "GET",
-      headers: {
-        ["x-api-key"]: API_KEY,
-      },
-    }
-  );
-  const adGroups = await adGroupsRes.json();
-  if (adGroups.length === 0) {
-    log("no oidc group with name found", group.group.name);
-    return;
-  }
-  await fetch(`${SERVER_URL}/api/v1/oidc/mapping`, {
-    method: "PUT",
-    headers: {
-      ["x-api-key"]: API_KEY,
-      ["Content-Type"]: "application/json",
-    },
-    body: JSON.stringify({ team: teamId, group: adGroups[0].uuid }),
-  });
-  /*
-  const cnEl = adGroups[0]
-    .split(",")
-    .map((el) => el.split("="))
-    .find((el) => el[0] === "CN");
-  if (!cnEl) {
-    return;
-  }
-  const cn = cnEl[1];
-  adGroupCns.push(cn);
-  // create sonar group
-  await fetch(`${SONAR_HOST_URL}/api/user_groups/create`, {
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-      Authorization:
-        "Basic " + Buffer.from(SONAR_LOGIN + ":").toString("base64"),
-    },
-    body: `description=&name=${cn}`,
-    method: "POST",
-  });
-  */
-}
